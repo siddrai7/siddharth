e5f1a12071905505aa2ed6069ed99fe9e4ae901b
Siddharth Rai <sidd35070@gmail.com>
2023-06-27 19:56:50 +0530

    added name animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import NameAnimation from "./components/NameAnimation/NameAnimation";
 
 function App() {
+  const [showAnimation, setShowAnimation] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShowAnimation(false);
+    }, 6500);
+  }, []);
   return (
     <div className="App">
+      {showAnimation ? <NameAnimation /> : ""}
       <Header />
       <Hero />
     </div>
